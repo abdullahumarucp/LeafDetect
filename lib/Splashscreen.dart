5e4b259b81882268774286bf56a83655ef5339f0
Abdullah Umar <116197698+abdullahumarucp@users.noreply.github.com>
2025-06-28 14:23:35 +0500

    I need to change this comment

--- lib/Splashscreen.dart
+++ lib/Splashscreen.dart
@@ -46,6 +46,7 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: GradientBackground(
         child: Center(
           child: AnimatedOpacity(
